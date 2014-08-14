367127916246d44652ae520cc58acd46c1b07036
tenny1028 <jasper@reddins-MBP-2011.local>
2014-08-14 12:51:42 -0500

    Initial Commit

---
+++ Line Insertions/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Line Insertions
+//
+//  Created by Jasper Reddin on 8/14/14.
+//  Copyright (c) 2014 TwoGuysBrickfilms. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification?) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification?) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Line Insertions/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Line Insertions
+//
+//  Created by Jasper Reddin on 8/14/14.
+//  Copyright (c) 2014 TwoGuysBrickfilms. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
